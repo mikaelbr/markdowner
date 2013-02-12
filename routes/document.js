3af0c05463a49fbfa1d1cf0315ad78a4469887a5
Mikael Brevik <mikaelbre@gmail.com>
2013-02-12 09:46:02 +0100

    Improved document sharer

--- routes/document.js
+++ routes/document.js
@@ -1,5 +1,6 @@
 var md = require("node-markdown").Markdown;
 var store = require('../lib/store'),
+    user = require('../lib/user'),
     structure = require('../lib/folderHierarchy');
 
 
@@ -13,13 +14,19 @@ exports.index = function(req, res){
             });
             return;
         }
-        store.get(id, function(err, data) {
-            res.render('document', { 
-                title: 'Express', 
-                doc: data,
-                md:md
+        var user_id = file.user_id;
+
+        user.get({'_id': user_id}, function (err, user) {
+            store.get(id, function(err, data) {
+                res.render('document', { 
+                    title: file.name, 
+                    doc: data,
+                    md:md,
+                    user: user,
+                    file: file
+                });
             });
-        });
+        })
     });
     
 };
