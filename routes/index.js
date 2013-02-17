d7a02fe6b233bb02ff57c42690749b9f8985a392
Mikael Brevik <mikaelb@mikaelb.net>
2013-02-17 14:02:49 +0100

    Now giving more info on serverside index.jade sideload.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,39 @@
+var async = require('async')
+  , store = require('../lib/store')
+  , structure = require('../lib/folderHierarchy');
 
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Markdowner', user: req.user });
+
+  var user = req.user
+    , activeDocumentId = user.settings.activeDocument.file_id;
+
+  async.series({
+    folders: function (done) {
+      structure.list(user._id, function (err, list) {
+        done(err, list);
+      });
+    }
+
+    , doc: function (done) {
+      if (!activeDocumentId) done(null, null);
+
+      store.get(req.params.document, function (err, item) {
+        done(err, item);
+      });
+    }
+
+  }
+
+  , function (err, res) {
+    res.render('index', { 
+      title: 'Markdowner' 
+    , user: user
+    , folders: res.folders
+    , documents: res.doc
+    });
+  });
 };
